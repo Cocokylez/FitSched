1de7a7b2f76e5f46062f07dcc5291565bc78b28b
Cocokylez <a.condeza.150843.tc@umindanao.edu.ph>
2026-05-28 14:25:30 +0800

    feat: add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { NativeShell } from "@/components/NativeShell";
 import { SwRegistration } from "@/components/SwRegistration";
 import { auth } from "@/lib/auth";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const APP_URL = "https://fitsched.vercel.app";
 
@@ -84,6 +85,7 @@ export default async function RootLayout({
           </SessionProvider>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
